782bf711257d426236f310bcac25f01b61ba26bb
Zhao Chen <Zhao@Zhaos-MacBook-Air.local>
2019-02-23 15:43:17 -0500

    added getMoboPrice and getMoboBrand

--- pc_empire/routes/api.js
+++ pc_empire/routes/api.js
@@ -16,6 +16,34 @@ router.get("/getAllMobos", (req, res, next) => {
         .then(data => res.json(data))
         .catch(next)
 });
+router.get("/getMoboBrand", (req, res, next) => {
+    //this will return all CPUs from the database
+    Mobo.find({},"Manufacturer")
+        .then(data => {
+            const result =[];
+            for (var i in data) {
+                var obj = JSON.parse(JSON.stringify(data[i]));
+                if(!result.includes(obj.Manufacturer))
+                    result.push(obj.Manufacturer);
+            }
+            res.json(result);
+        })
+        .catch(next)
+});
+router.get("/getMoboPrice", (req, res, next) => {
+    //this will return all CPUs from the database
+    Mobo.find({},"Price")
+        .then(data => {
+            const result =[];
+            for (var i in data) {
+                var obj = JSON.parse(JSON.stringify(data[i]));
+                if(!result.includes(obj.Price))
+                    result.push(obj.Price);
+            }
+            res.json(result.sort(function(a, b){return a-b}));
+        })
+        .catch(next)
+});
 /*
 router.post('/todos', (req, res, next) => {
   if(req.body.action){
