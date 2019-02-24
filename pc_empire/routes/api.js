d6e05b191d76374acf092a120522744e5f8f1084
philip <philiprachwalski@hotmail.ca>
2019-02-23 22:57:27 -0500

    added more filter for CPU

--- pc_empire/routes/api.js
+++ pc_empire/routes/api.js
@@ -26,6 +26,94 @@ router.get("/getCPUBrand", (req, res, next) => {
         })
         .catch(next)
 });
+
+
+router.get("/getCPUName", (req, res, next) => {
+    //this will return all CPUs from the database
+    CPU.find({},"Name")
+        .then(data => {
+            const result =[];
+          for (var i in data) {
+              var obj = JSON.parse(JSON.stringify(data[i]));
+              if(!result.includes(obj.Name))
+                  result.push(obj.Name);
+          }
+          res.json(result);
+        })
+        .catch(next)
+});
+
+router.get("/getCPUSocket", (req, res, next) => {
+    //this will return all CPUs from the database
+    CPU.find({},"Socket")
+        .then(data => {
+            const result =[];
+          for (var i in data) {
+              var obj = JSON.parse(JSON.stringify(data[i]));
+              if(!result.includes(obj.Socket))
+                  result.push(obj.Socket);
+          }
+          res.json(result);
+        })
+        .catch(next)
+});
+
+router.get("/getCPUClock", (req, res, next) => {
+    //this will return all CPUs from the database
+    CPU.find({},"Clock")
+        .then(data => {
+            const result =[];
+          for (var i in data) {
+              var obj = JSON.parse(JSON.stringify(data[i]));
+              if(!result.includes(obj.Clock))
+                  result.push(obj.Clock);
+          }
+          res.json(result);
+        })
+        .catch(next)
+});
+router.get("/getCPUCores", (req, res, next) => {
+    //this will return all CPUs from the database
+    CPU.find({},"Clock")
+        .then(data => {
+            const result =[];
+          for (var i in data) {
+              var obj = JSON.parse(JSON.stringify(data[i]));
+              if(!result.includes(obj.Cores))
+                  result.push(obj.Cores);
+          }
+          res.json(result.sort(function(a, b){return a-b}));
+        })
+        .catch(next)
+});
+router.get("/getCPUPower", (req, res, next) => {
+    //this will return all CPUs from the database
+    CPU.find({},"Power")
+        .then(data => {
+            const result =[];
+          for (var i in data) {
+              var obj = JSON.parse(JSON.stringify(data[i]));
+              if(!result.includes(obj.Power))
+                  result.push(obj.Power);
+          }
+          res.json(result.sort(function(a, b){return a-b}));
+        })
+        .catch(next)
+});
+router.get("/getCPUStock_Cooler", (req, res, next) => {
+    //this will return all CPUs from the database
+    CPU.find({},"Power")
+        .then(data => {
+            const result =[];
+          for (var i in data) {
+              var obj = JSON.parse(JSON.stringify(data[i]));
+              if(!result.includes(obj.Stock_Cooler))
+                  result.push(obj.Stock_Cooler);
+          }
+          res.json(result);
+        })
+        .catch(next)
+});
 router.get("/getCPUPrice", (req, res, next) => {
     //this will return all CPUs from the database
     CPU.find({},"Price")
