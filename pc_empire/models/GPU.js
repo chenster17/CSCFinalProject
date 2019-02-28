df7a7026fdc713eca1320f732b53e981e453a81a
philip rachwalski <philiprachwalski@hotmail.ca>
2019-02-28 13:44:23 -0500

    164311912: Schema creaded for Case and GPU

---
+++ pc_empire/models/GPU.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//create schema for pc_empire
+const GPUSchema = new Schema({
+    ChipsetManufacturer: String,
+    Manufacturer: String,
+    Name: String,
+    Clock: Number,
+    Vram: Number,
+    Interface: String,
+    Power, Number,
+    Price: Number
+});
+
+//create model for pc_empire
+
+const GPU = mongoose.model('GPU', CaseSchema,'GPU');
+
+module.exports = GPU;
