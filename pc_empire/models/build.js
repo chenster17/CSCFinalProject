b610dc64422d3db2e76ea48f36146f7da1a873dc
philip rachwalski <philiprachwalski@hotmail.ca>
2019-03-06 18:33:22 -0500

    164347857 created Skema for System build

---
+++ pc_empire/models/build.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//create schema for pc_empire
+const BuildSchema = new Schema({
+    Owner_id: String,
+    Build_Name: String,
+    CPU_id: String,
+    Case_id: String,
+    GPU_id: String,
+    Motherboard_id: String,
+    Power_Supply_id: String,
+    RAM_id: String,
+    Storage_id: String,
+    Reviews: Array,
+    Comments: Array
+});
+
+//create model for pc_empire
+
+const Build = mongoose.model('Build', BuildSchema,'Build');
+
+module.exports = Build;
